a6f5631a8bc7c8383e7d1e6ec8fa3350177c6ce4
dmm20210201 <tnaoki.09j@gmail.com>
2021-03-04 09:04:42 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
 
+  before_action :correct_user, only: [:edit, :update]
+
   def show
     @book = Book.find(params[:id])
     @new = Book.new
@@ -49,6 +51,14 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def correct_user
+    book = Book.find(params[:id])
+    user = book.user
+    if current_user != user
+      redirect_to books_path
+    end
   end
 
 end
